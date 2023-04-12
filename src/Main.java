b4530c8157dbadd5efafb156a30e09ed9a4c7eb7
Owen Blount <psyob1@nottingham.ac.uk>
2023-04-12 19:04:21 +0100

    Added time budget for termination

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,13 @@ public class Main
     {
         Instance d1 = new Instance("d1_50_500", 24042023);
         d1.CreateSolution();
-        boolean terminationCriteria = true;
-        while(terminationCriteria)
+
+        final long timeBudget = 10L * (1000 * 1000 * 1000);
+        final long startTime = System.nanoTime();
+        while ((System.nanoTime() - startTime) < timeBudget)
         {
             RandomBitFlipHeuristic randomBitFlip = new RandomBitFlipHeuristic(d1.rnd);
             randomBitFlip.ApplyHeuristic(d1);
-            if(d1.GetObjectiveValue() <= d1.GetNumberOfVariables())
-                terminationCriteria = false;
         }
         System.out.println(d1.GetCurrentSolutionAsString());
         System.out.println(d1.GetObjectiveValue());
